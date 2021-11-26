064fa0cf3f322bdc14563be635bb564e5ceb75ee
Giorgio Andrei <cipuandrei@libero.it>
2021-11-26 20:41:08 +0100

    components class added for dynamic components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 //^ LA MIA PAGINA PRINCIPALE
 
+//? IMPORTO LA CLASSE COMPONENT PERCHE' LA UTILIZZO NEL COMPONENTE APP
+import React, { Component } from 'react';
+
 //? I FILE SI IMPORTANO COSI, COME SU VUE.JS
 import './App.css';
 import Navbar from './components/navbar';
 import Card from './components/card';
 
-function App() {
-  return (
-    <>
-      {/* //^SE VOGLIO METTERE PIU TAGS ACCANTO DEVO RACCHIUDERLI NEL FRAGMENT JSX ( <> </> )*/}
-      <Navbar />
-      <div className="container mt-5">
-        <div className="row">
-          <Card />
+//^ SE DEVO MOSTRARE VALORI DINAMICAMENTE E PASSARE DEI DATI FRA COMPONENTI
+//^ UTILIZZO IL CLASS COMPONENT 
+class App extends Component {
+  //^ FUNZIONE DI RENDER PER I CLASS COMPONENT
+  render() {
+    return (
+      <>
+        {/* //^SE VOGLIO METTERE PIU TAGS ACCANTO DEVO RACCHIUDERLI NEL FRAGMENT JSX ( <> </> )*/}
+        < Navbar />
+        <div className="container mt-5">
+          <div className="row">
+            <Card />
+
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 //^UNA VOLTA DEFINITO CON LA FUNZIONE SOVRASTANTE, IL FILE VA ESPORTATO COSI
--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,23 @@
-function Card() {
-    return (
-        <div className="col">
-            <div className="card" style={{ width: '18rem' }}>
-                {/* //^Mettere il selfclosing tag ad img e l'attributo style va dato come fosse un oggetto */}
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+import React, { Component } from 'react';
+
+//^NOME DELLA FUNZIONE JS CON LO STESSO NOME DEL COMPONENTE
+class Card extends Component {
+    render() {
+        return (
+            <div className="col">
+                <div className="card" style={{ width: '18rem' }}>
+                    {/* //^Mettere il selfclosing tag ad img e l'attributo style va dato come fosse un oggetto */}
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button href="#" clbuttonssName="btn btn-primary">Delete</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    );
+        );
+    }
 }
 
 export default Card;
