b4cc32164adee2fede09b0e3d961cdfc2c4cdc11
Giorgio Andrei <cipuandrei@libero.it>
2021-12-06 11:43:42 +0100

    card props passed dynamically

--- src/App.js
+++ src/App.js
@@ -11,6 +11,21 @@ import Card from './components/card';
 //^ SE DEVO MOSTRARE VALORI DINAMICAMENTE E PASSARE DEI DATI FRA COMPONENTI
 //^ UTILIZZO IL CLASS COMPONENT 
 class App extends Component {
+  //^ DICHIARO LA VARIABILE STATE COME ARRAY DI OGGETTI CHE SARANNO POI LE PROPS
+  //^ CHE PASSERÒ IN MODO CICLICO NEI COMPONENTI
+  state = {
+    cards: [
+      { id: 1, name: "Kiwi", price: 2.99, image: "https://www.longacres.co.uk/Images/Product/Default/xlarge/fresh-fruit-and-vegetables-kiwi-single.jpg", quantity: 0 },
+      { id: 2, name: "Banana", price: 0.99, image: "https://image.shutterstock.com/image-photo/banana-isolated-260nw-366770894.jpg", quantity: 0 },
+      { id: 3, name: "Apple", price: 0.99, image: "https://image1.masterfile.com/getImage/NjU5LTAzNTI3NzUxZW4uMDAwMDAwMDA=ABu8x2/659-03527751en_Masterfile.jpg", quantity: 0 },
+      { id: 4, name: "Watermelon", price: 3.49, image: "https://img.etimg.com/thumb/msid-64446447,width-640,resizemode-4,imgsize-364438/densuke-watermelons.jpg", quantity: 0 },
+      { id: 5, name: "Pineapple", price: 4.99, image: "https://www.kindpng.com/picc/m/68-682668_pineapple-png-background-single-fruits-and-vegetables-transparent.png", quantity: 0 },
+      { id: 6, name: "Avocado", price: 3.99, image: "https://farmingbase.com/wp-content/uploads/2020/10/Which-fruit-has-a-single-seed-Apricot-00.jpg", quantity: 0 },
+      { id: 7, name: "Grapes", price: 1.99, image: "https://www.pngitem.com/pimgs/m/42-427313_single-fruits-and-vegetables-hd-png-download.png", quantity: 0 },
+      { id: 8, name: "Coconut", price: 4.99, image: "https://previews.123rf.com/images/boarding1now/boarding1now1607/boarding1now160700058/61743595-coconut-coconuts-fruit-fresh-fruits-isolated-on-a-white-background.jpg", quantity: 0 },
+    ]
+  }
+
   //^ FUNZIONE DI RENDER PER I CLASS COMPONENT
   render() {
     return (
@@ -19,8 +34,15 @@ class App extends Component {
         < Navbar />
         <div className="container mt-5">
           <div className="row">
-            <Card />
-
+            {/* CICLO SULLA CARDA CON LA FUNZIONE MAP CHE HA COME PARAMETRO PROPRIO LA CARTA */}
+            {this.state.cards.map(card => (
+              <Card
+                key={card.id}
+                image={card.image}
+                name={card.name}
+                price={card.price}
+                quantity={card.quantity} />
+            ))}
           </div>
         </div>
       </>
--- src/components/card.js
+++ src/components/card.js
@@ -4,14 +4,16 @@ import React, { Component } from 'react';
 class Card extends Component {
     render() {
         return (
-            <div className="col">
-                <div className="card" style={{ width: '18rem' }}>
+            <div className="col mb-5">
+                <div className="card bg-light" style={{ width: '18rem' }}>
                     {/* //^Mettere il selfclosing tag ad img e l'attributo style va dato come fosse un oggetto */}
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src={this.props.image} style={{ maxWidth: '18rem', height: '216px', objectFit: 'contain' }} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button href="#" clbuttonssName="btn btn-primary">Delete</button>
+                        {/* IN REACT LE PROPS RICEVUTE DAL COMPONENTE PADRE SI SCRIVONO COSI NEL COMPONENTE FIGLIO */}
+                        <h5 className="card-title">{this.props.name}</h5>
+                        <p className="card-text">€{this.props.price}</p>
+                        <button className="me-3">{this.props.quantity}</button>
+                        <button href="#" className="btn btn-primary">Delete</button>
                     </div>
                 </div>
             </div>
